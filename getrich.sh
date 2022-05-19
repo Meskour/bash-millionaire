810230445f772b0795619e1701a6dca5b20d5685
Meskour Mohamed <sotradia@gmail.com>
2022-05-19 05:32:31 +0100

    Adding the bash script magic

---
+++ getrich.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+echo "What is your name: "
+read name
+
+echo "How old are you: "
+read age
+
+sleep 2
+
+getrich=$((( $RANDOM % 15 ) + $age))
+
+echo "$name, you will become a millionaire when you are $getrich years old!"
